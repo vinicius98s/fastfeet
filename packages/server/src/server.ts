298b0d3a0658f8b14eec737dffc07f553ac8bd36
Vinicius Sales <vinicius.2010.s@gmail.com>
2020-03-13 19:59:39 -0300

    chore: Setup Docker and database for development

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from 'express';
-
+import 'dotenv/config';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello');
+  console.log(process.env.DB_HOST);
+  res.send('Hello World!');
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+  console.log(`🚀 Running on port ${port}`);
 });
